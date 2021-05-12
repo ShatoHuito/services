8eca5ffed1fc452235fcb901fa052e213184dd65
gbrittan <shatohuito0605@gmail.com>
2021-05-12 19:33:08 +0300

    123 comm

--- phpmyadmin/srcs/config.inc.php
+++ phpmyadmin/srcs/config.inc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-$cfg['blowfish_secret'] = 'qwerty123123ytrewq'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'qwerty123123ytrelkjnblkjhgfkjhgpokjhgpoiuypoiuypoiuywq'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 
 $i = 0;
@@ -13,10 +13,11 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql-service';
+$cfg['Servers'][$i]['host'] = 'mysql-svc';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 $cfg['Servers'][$i]['port'] = '3306';
+$cfg['TempDir'] = '/tmp';
 
 
 $cfg['Servers'][$i]['user'] = 'qwerty';
--- start.sh
+++ start.sh
@@ -16,9 +16,19 @@ docker build -t nginx_image ./nginx/
 docker build -t php_image ./phpmyadmin/
 docker build -t mysql_image ./mysql/
 docker build -t wordpress_image ./wordpress/
+docker build -t ftps_image ./ftps/
+docker build -t influx_image ./influxdb/
+docker build -t grafana_image ./grafana/
+
+
 
 
 kubectl apply -f ./nginx/nginx.yaml
 kubectl apply -f ./phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./mysql/mysql.yaml
 kubectl apply -f ./wordpress/wordpress.yaml
+kubectl apply -f ./ftps/ftps.yaml
+kubectl apply -f ./influxdb/influxdb.yaml
+kubectl apply -f ./grafana/grafana.yaml
+
+
